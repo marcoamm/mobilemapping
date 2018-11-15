41b1e56585af250f790463fab2a97a33a65e7ca4
Marco <marco.mendonca@unb.ca>
2018-11-14 23:14:30 -0200

    Fixed high and low levels

--- iocontrol.py
+++ iocontrol.py
@@ -5,19 +5,23 @@ import subprocess
 GPIO.setmode(GPIO.BCM)
 
 #Button one - GPS/IMU on
-GPIO.setup(22, GPIOIN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 #Button 2 - Beep sound
-GPIO.setup(27, GPIOIN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 #LED GPS and IMU data collection
-GPIO.setup(17, GPIOIN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 while True:
     gps  = GPIO.input(22)
     beep = GPIO.input(27)
     led  = GPIO.input(17)
-    if beep == False:
-        subprocess.call("python3 /home/pi/mobilemapping/main.py", shell=True)
+    if beep == True:
+        subprocess.call("python3 /home/pi/Documents/mobilemapping/main.py", shell=True)
+    if gps == True:
+        GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        time.sleep(5)
+        GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     time.sleep(0.2)
